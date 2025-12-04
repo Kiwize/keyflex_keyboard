6e7017c8cafd93941c8cf6063fd1ec3089e9bd58
Thomas PRADEAU <thomas@avnear.fr>
2025-12-04 11:25:45 +0100

    fix: footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,12 +4,12 @@ const Footer = () => {
   return (
     <footer className="footer">
       <div className="container footer-layout">
-        <div>© {new Date().getFullYear()} — Clavier dynamique E-Ink.</div>
-        <div className="footer-links">
+        <div>© {new Date().getFullYear()} — KeyFlex</div>
+        {/* <div className="footer-links">
           <a href="#">Mentions légales</a>
           <a href="#">Confidentialité</a>
           <a href="#">Contact</a>
-        </div>
+        </div> */}
       </div>
     </footer>
   );
